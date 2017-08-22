55604080dc401dffe59421351df81c53ea9bfaee
Jonathan Peters <peters@lucasware.com>
2017-08-22 11:04:27 -0400

    setup angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {MdCardModule, MdToolbarModule, MdMenuModule, MdIconModule, MdButtonModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdIconModule,
+    MdButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
