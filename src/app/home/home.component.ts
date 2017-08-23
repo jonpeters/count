1ec6685af9e493ca5674a2e067c0a80ee679a2a8
Jonathan Peters <peters@lucasware.com>
2017-08-23 15:37:58 -0400

    add some temporary data on the client to test the layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Category} from "../model/category";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,15 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  categories: Array<Category> = new Array<Category>();
+
   ngOnInit() {
+    for (var i=0; i<10; i++) {
+      this.categories.push({
+        _id: null,
+        name: "Category " + String.fromCharCode(i+65)
+      });
+    }
   }
 
 }
