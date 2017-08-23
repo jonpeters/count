8c970d733261d23bc8870823d0e91a5d1242c536
Jonathan Peters <peters@lucasware.com>
2017-08-22 23:30:48 -0400

    add sidenav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MdCardModule, MdToolbarModule, MdMenuModule, MdIconModule, MdButtonModule } from "@angular/material";
+import {
+  MdCardModule, MdToolbarModule, MdMenuModule, MdIconModule, MdButtonModule,
+  MdSidenavModule
+} from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from './home/home.component';
@@ -32,6 +35,7 @@ const routes: Routes = [{
     MdMenuModule,
     MdIconModule,
     MdButtonModule,
+    MdSidenavModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
