68bf2d46e43cb62a28a9d2847438a7598c788abe
Jonathan Peters <peters@lucasware.com>
2017-08-25 14:19:20 -0400

    set a return type on the service method

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -8,7 +8,7 @@ export class CategoryService {
 
   constructor(private http: Http) { }
 
-  public createNewCategory(category: Category)  {
+  public createNewCategory(category: Category) : Observable<Category> {
     // TODO is this the best way to infer the path or is there something better that maybe Angular provides?
     return this.http.post(`${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}/api/category`, category)
       .map((response: Response) => response.json())
